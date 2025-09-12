c4cf7dc87a71a109a7199e701e104e8cf7c661a1
sanjith3 <sanjithmit@gmail.com>
2025-09-12 15:48:06 +0530

    followup commit

--- adsoft/settings.py
+++ adsoft/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
