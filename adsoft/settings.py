71d0feb1fa924445e0ce795ba744b9bf4d33f07b
sanjith3 <sanjithmit@gmail.com>
2025-10-03 17:25:56 +0530

    Revert "merge"
    This reverts commit 523e7f70bfd4288ad2190d87032cd7f51a4bd171.

--- adsoft/settings.py
+++ adsoft/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'campaigns',
-    # "tailwind",
+    "tailwind",
     # "theme", 
     # "django_browser_reload",
 ]
@@ -43,10 +43,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # "django_browser_reload.middleware.BrowserReloadMiddleware",
     'adsoft.middleware.SessionErrorMiddleware',
 ]
 
+TAILWIND_APP_NAME = "theme"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 ROOT_URLCONF = 'adsoft.urls'
 
